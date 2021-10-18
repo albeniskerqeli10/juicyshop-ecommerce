105ebc588debfb4f07798751a2e69ded99dc2aab
Albenoo <albeniskerqeli5@gmail.com>
2021-10-18 16:57:32 +0200

    v

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use('/api/orders' , orderRoutes);
 app.get('/api/config/paypal' , (req,res) => {
     res.send(process.env.PAYPAL_CLIENT_ID);
 })
-const PORT =  process.env.PORT || 5000;
+const PORT =  process.env.PORT || 4000;
 app.listen(PORT ,() => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 })
