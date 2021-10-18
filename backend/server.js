f2b79a18956cc5c6d857d87f2cd7df43f1c2aaaf
Albenoo <albeniskerqeli5@gmail.com>
2021-10-18 17:01:26 +0200

    s

--- backend/server.js
+++ backend/server.js
@@ -19,15 +19,9 @@ app.use(cors());
 app.use((req,res,next) => {
     next();
 })
-
-app.get('/' , (req,res) => {
-  res.send('Hello');
-})
-
-
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static("client/build"));
-    app.get("*", (req,res) => {
+    app.get("/", (req,res) => {
         res.sendFile(path.resolve(__dirname , "../client"  , "build" , "index.html"));
     })
 }
